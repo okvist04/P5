8d653571c2600d4ff13aa0e1022de741431fd7d9
Olivia Kvist <okvist18@student.aau.dk>
2020-12-10 11:01:03 +0100

    Updated

--- Linmodel_P5.R
+++ Linmodel_P5.R
@@ -1,39 +1,37 @@
 library(readxl)
 my.data <- read_excel("my.data.xlsx")
-install.packages("Metrics")
 #Packages needed for the code
 library(car); library(carData); library(Metrics)
 
 #Checking for outliers
-model <- lm(deaths_per ~. -Country -deaths_per, data = my.data)
+model <- lm(deaths_per ~. -Country -deaths_per -Population, data = my.data)
 stud.res <- rstudent(model) 
 plot(predict(model), stud.res) 
 qqnorm(stud.res); qqline(stud.res, col = "red")
-outlier <- which.max(stud.res); stud.res[outlier] #Outlier is no. 6 = 3.0053
-deaths <- as.vector(my.data$deaths_per)
-cov1 <- cov(coef(model))
-mse <- rmse(deaths, predict(model))
+outlier <- which.max(stud.res); stud.res[outlier] #Outlier is no. 40 = 3.0404
+par(mfrow = c(2,2))
+plot(model)
 
-matrix <- as.matrix(my.data[,-1])
-hat <- matrix%*%inv((t(matrix)%*%matrix))%*%t(matrix)
-
-#Model defined without Belgium, i.e. no. 6
-model1 <- lm(deaths_per ~. -Country -deaths_per, data = my.data[c(-6),])
+#Model defined without Peru, i.e. no. 40
+model1 <- lm(deaths_per ~. -Country -deaths_per -Population, data = my.data[c(-40),])
 stud.res1 <- rstudent(model1)
+par(mfrow = c(2,2))
+plot(model1)
 plot(predict(model1), stud.res1)
 qqnorm(stud.res1); qqline(stud.res1, col = "red")
-outlier1 <- which.max(stud.res1); stud.res1[outlier1] #Outlier no. 40 = 3.0566
+outlier1 <- which.max(stud.res1); stud.res1[outlier1] #Outlier no. 6 = 3.1296
+hatvalues(model1)
 
-#Model defined without Pery, i.e. no. 40
-model2 <- lm(deaths_per ~. -Country -deaths_per, data = my.data[c(-6,-40),])
+#Model defined without Belgium, i.e. no. 6
+model2 <- lm(deaths_per ~. -Country -deaths_per -Population, data = my.data[c(-40,-6),])
 stud.res2 <- rstudent(model2)
 plot(predict(model2), stud.res2)
 qqnorm(stud.res2); qqline(stud.res2, col = "red")
-outlier2 <- which.max(stud.res2); stud.res2[outlier2] #QQ-plot was better with Norway that without
+outlier2 <- which.max(stud.res2); stud.res2[outlier2] #QQ-plot was better with Belgium than without
 #Model1 is used.
 
 #Checking for VIF > 5, and removing one by one until all has VIF <= 5
-vifmodel <- lm(deaths_per ~ . -Country -deaths_per, data = my.data[c(-6),])
+vifmodel <- lm(deaths_per ~ . -Country -deaths_per -Population, data = my.data[c(-40),])
 all_vifs <- vif(vifmodel); all_vifs
 signifvif_all <- names(all_vifs)
 
@@ -42,7 +40,7 @@ while(any(all_vifs > 5)){
   signifvif_all <- signifvif_all[!(signifvif_all) %in% var_with_max_vif]  # remove
   myForm <- as.formula(paste("deaths_per ~ ", 
                              paste (signifvif_all, collapse=" + "), sep=""))  # new formula
-  model3 <- lm(myForm, data=my.data)  # re-build model with new formula
+  model3 <- lm(myForm, data=my.data[c(-40),])  # re-build model with new formula
   all_vifs <- vif(model3)
 }
 summary(model3)
@@ -62,124 +60,90 @@ plot(stand.res3, main = "Residuals")
 yhat3 <- predict(model3); yhat3 
 par(mfrow = c(1,1))
 plot(yhat3, stand.res3) #Looks randomnized
+#Diagnostics plots 
+par(mfrow = c(2,2))
+plot(model3)
+#Fitted values 
+summary(model3)
 
 #Plotting all explanatory variables against residuals, to see if some needs to be transformed
-par(mfrow = c(3,4))
-plot(my.data$Population, stand.res3)
-plot(my.data$Pop.density, stand.res3)
-plot(my.data$GDP, stand.res3)
-plot(my.data$cardiovasc_per, stand.res3)
-plot(my.data$diabetes_per, stand.res3)
-plot(my.data$Smokers, stand.res3)
-plot(my.data$COPD_per, stand.res3)
-plot(my.data$CKD_per, stand.res3)
-plot(my.data$health_exp, stand.res3)
-plot(my.data$Obesity, stand.res3)
-plot(my.data$organ_per, stand.res3)
-plot(my.data$asthma_per, stand.res3)
-plot(my.data$Gini, stand.res3)
+par(mfrow = c(2,2))
+plot(my.data[c(-40),]$Pop.density, stand.res3)
+plot(my.data[c(-40),]$GDP, stand.res3)
+plot(my.data[c(-40),]$cardiovasc_per, stand.res3)
+plot(my.data[c(-40),]$diabetes_per, stand.res3)
+plot(my.data[c(-40),]$Smokers, stand.res3)
+plot(my.data[c(-40),]$COPD_per, stand.res3)
+plot(my.data[c(-40),]$CKD_per, stand.res3)
+plot(my.data[c(-40),]$health_exp, stand.res3)
+plot(my.data[c(-40),]$Obesity, stand.res3)
+plot(my.data[c(-40),]$organ_per, stand.res3)
+plot(my.data[c(-40),]$asthma_per, stand.res3)
+plot(my.data[c(-40),]$Gini, stand.res3)
 
-#Population and pop.density depends on the residuals, i.e. log-transform these 
-log.model <- lm(deaths_per ~ log(Population) + log(Pop.density) + GDP + cardiovasc_per
-                 + diabetes_per + Smokers + COPD_per + CKD_per + health_exp + Obesity + asthma_per + Gini,
-                 data = my.data[c(-6),])
-summary(log.model)
-log.res <- residuals(log.model)
-stand.logres <- rstandard(log.model)
-stud.logres <- rstudent(log.model)
-
+#Histograms with normal curve is plotted for deaths
 par(mfrow = c(1,1))
-qqnorm(stand.logres); qqline(stand.logres, col = "red")
-plot(predict(log.model), stand.logres)
-
-#Plots of residuals and explanatory variables 
-par(mfrow = c(3,4))
-plot(log(my.data[c(-6),]$Population), stand.logres)
-plot(log(my.data[c(-6),]$Pop.density), stand.logres)
-plot(my.data[c(-6),]$GDP, stand.logres)
-plot(my.data[c(-6),]$cardiovasc_per, stand.logres)
-plot(my.data[c(-6),]$diabetes_per, stand.logres)
-plot(my.data[c(-6),]$Smokers, stand.logres)
-plot(my.data[c(-6),]$COPD_per, stand.logres)
-plot(my.data[c(-6),]$CKD_per, stand.logres)
-plot(my.data[c(-6),]$health_exp, stand.logres)
-plot(my.data[c(-6),]$Obesity, stand.logres)
-plot(my.data[c(-6),]$organ_per, stand.logres)
-plot(my.data[c(-6),]$asthma_per, stand.logres)
-plot(my.data[c(-6),]$Gini, stand.logres) #The dependence is gone 
-
-#Histograms with normal curve is plotted for deaths and log(deaths)
-par(mfrow = c(1,2))
-hist(my.data[c(-6),]$deaths_per, prob = TRUE, xlim = c(-0.15,0.15)); 
-curve(dnorm(x, mean(my.data[c(-6),]$deaths_per), sd(my.data[c(-6),]$deaths_per)), 
+hist(my.data[c(-40),]$deaths_per, prob = TRUE, xlim = c(-0.15,0.15)); 
+curve(dnorm(x, mean(my.data[c(-40),]$deaths_per), sd(my.data[c(-40),]$deaths_per)), 
       add = TRUE, col = "green") #The distribution of deaths is right-skewed
 
 #The Q-Q plot of the standard residuals is made, to check if the residuals are approx. normally dist.
-par(mfrow = c(2,1))
-qqnorm(stand.res3); qqline(stand.res3, col = "red")
-qqnorm(stand.logres); qqline(stand.logres, col = "red")
-
-#Diagnostics plots for the log-model
-par(mfrow = c(2,2))
-plot(log.model)
 par(mfrow = c(1,1))
-plot(stud.logres)
+qqnorm(stand.res3); qqline(stand.res3, col = "red") #Looks fine 
+hist(stand.res3, prob = TRUE); curve(dnorm(x, mean(stand.res3), sd(stand.res3)), 
+                                     add = TRUE, col = "green") #Shows some heavy tails 
 
 #Next, it's checked if the model can be reduced by removing insignificant p-values one by one
-first.p <- which.max(summary(log.model)$coefficients[,4]); 
-summary(log.model)$coefficients[,4][first.p] #Obesity biggest, removed
+first.p <- which.max(summary(model3)$coefficients[,4]); 
+summary(model3)$coefficients[,4][first.p] #Obesity biggest, removed, = 0.6460
 
-log.mod <- lm(deaths_per ~ log(Population) + log(Pop.density) + GDP + cardiovasc_per
-                 + diabetes_per + Smokers + COPD_per + CKD_per + health_exp + asthma_per + Gini,
-                 data = my.data[c(-6),])
-sec.p <- which.max(summary(log.mod)$coefficients[,4]); 
-summary(log.mod)$coefficients[,4][sec.p] #COPD_per biggest, removed
+model4 <- lm(deaths_per ~ Pop.density + GDP + cardiovasc_per + diabetes_per
+             + Smokers + COPD_per + CKD_per + health_exp + organ_per
+             + asthma_per + Gini, data = my.data[c(-40),])
+sec.p <- which.max(summary(model4)$coefficients[,4]); 
+summary(model4)$coefficients[,4][sec.p] #Pop.density biggest, removed, = 0.5822
 
-log.mod1 <- lm(deaths_per ~ log(Population) + log(Pop.density) + GDP + cardiovasc_per
-              + diabetes_per + Smokers + CKD_per + health_exp + asthma_per + Gini, data = my.data[c(-6),])
-third.p <- which.max(summary(log.mod1)$coefficients[,4]); 
-summary(log.mod1)$coefficients[,4][third.p] #Smokers, p-val = 0.7129
+model5 <- lm(deaths_per ~ GDP + cardiovasc_per + diabetes_per + Smokers + COPD_per + CKD_per 
+             + health_exp + organ_per + asthma_per + Gini, data = my.data[c(-40),])
+third.p <- which.max(summary(model5)$coefficients[,4]); 
+summary(model5)$coefficients[,4][third.p] #asthma_per, p-val = 0.5812
 
-log.mod2 <- lm(deaths_per ~ log(Population) + log(Pop.density) + GDP + cardiovasc_per
-               + diabetes_per + CKD_per + health_exp + asthma_per + Gini, data = my.data[c(-6),])
-fourth.p <- which.max(summary(log.mod2)$coefficients[,4]); 
-summary(log.mod2)$coefficients[,4][fourth.p] #cardiovasc, p-val = 0.7369
+model6 <- lm(deaths_per ~ GDP + cardiovasc_per + diabetes_per + Smokers + COPD_per + CKD_per 
+             + health_exp + organ_per + Gini, data = my.data[c(-40),])
+fourth.p <- which.max(summary(model6)$coefficients[,4]); 
+summary(model6)$coefficients[,4][fourth.p] #health_exp, p-val = 0.5791
 
-log.mod3 <- lm(deaths_per ~ log(Population) + log(Pop.density) + GDP + diabetes_per
-               + CKD_per + health_exp + asthma_per + Gini, data = my.data[c(-6),])
-fifth.p <- which.max(summary(log.mod3)$coefficients[,4]); 
-summary(log.mod3)$coefficients[,4][fifth.p] #log(pop.density), p-val = 0.7711
+model7 <- lm(deaths_per ~ GDP + cardiovasc_per + diabetes_per + Smokers + COPD_per + CKD_per 
+             + organ_per + Gini, data = my.data[c(-40),])
+fifth.p <- which.max(summary(model7)$coefficients[,4]); 
+summary(model7)$coefficients[,4][fifth.p] #Smokers, p-val = 0.5627
 
-log.mod4 <- lm(deaths_per ~ log(Population) + GDP + diabetes_per
-               + CKD_per + health_exp + asthma_per + Gini, data = my.data[c(-6),])
-sixth.p <- which.max(summary(log.mod4)$coefficients[,4]); 
-summary(log.mod4)$coefficients[,4][sixth.p] #log(population), p-val = 0.4178
+model8 <- lm(deaths_per ~ GDP + cardiovasc_per + diabetes_per + COPD_per + CKD_per 
+             + Gini, data = my.data[c(-40),])
+sixth.p <- which.max(summary(model8)$coefficients[,4]); 
+summary(model8)$coefficients[,4][sixth.p] #cardiovsc_per, p-val = 0.8815
 
-log.mod5 <- lm(deaths_per ~ GDP + diabetes_per + CKD_per + health_exp + asthma_per + Gini, 
-               data = my.data[c(-6),])
-sev.p <- which.max(summary(log.mod5)$coefficients[,4]); 
-summary(log.mod5)$coefficients[,4][sev.p] #asthma_per, p-val = 0.3262
+model9 <- lm(deaths_per ~ GDP + diabetes_per + COPD_per + CKD_per + Gini, 
+               data = my.data[c(-40),])
+sev.p <- which.max(summary(model9)$coefficients[,4]); 
+summary(model9)$coefficients[,4][sev.p] #GDP, p-val = 0.4213
 
-log.mod6 <- lm(deaths_per ~ GDP + diabetes_per + CKD_per + health_exp + Gini,
-               data = my.data[c(-6),])
-eigth.p <- which.max(summary(log.mod6)$coefficients[,4]); 
-summary(log.mod6)$coefficients[,4][eigth.p] #GDP, p-val = 0.4460
+mod <- lm(deaths_per ~ diabetes_per + COPD_per + CKD_per + Gini,
+               data = my.data[c(-40),])
+eigth.p <- which.max(summary(mod)$coefficients[,4]); 
+summary(mod)$coefficients[,4][eigth.p] #diabetes_per, p-val = 0.2973
 
-log.mod7 <- lm(deaths_per ~ diabetes_per + CKD_per + health_exp + Gini, data = my.data[c(-6),])
-ninth.p <- which.max(summary(log.mod7)$coefficients[,4]); 
-summary(log.mod7)$coefficients[,4][ninth.p] #diabetes_per, p-val = 0.18
+mod1 <- lm(deaths_per ~ COPD_per + CKD_per + Gini, data = my.data[c(-40),])
+ninth.p <- which.max(summary(mod1)$coefficients[,4]); 
+summary(mod1)$coefficients[,4][ninth.p] #CKD_per, p-val = 0.1676
 
-log.mod8 <- lm(deaths_per ~ CKD_per + health_exp + Gini, data = my.data[c(-6),])
-ten.p <- which.max(summary(log.mod8)$coefficients[,4]); 
-summary(log.mod8)$coefficients[,4][ten.p] #intercept, this isn't removed, hence CKD removed instead
-summary(log.mod8)
+mod2 <- lm(deaths_per ~ COPD_per + Gini, data = my.data[c(-40),])
+ten.p <- which.max(summary(mod2)$coefficients[,4]); 
+summary(mod2)$coefficients[,4][ten.p] #All has p-val < 0.05, i.e. this is the final reduced model
+summary(mod2)
 
-log.mod9 <- lm(deaths_per ~ health_exp + Gini, data = my.data[c(-6),])
-elev.p <- which.max(summary(log.mod9)$coefficients[,4]); elev.p 
-summary(log.mod9) #All p-val < 0.08
-
-predict(log.mod9)
+predict(mod2) #Fitted values
 par(mfrow = c(2,2))
-plot(log.mod9)
-anova(log.model, log.mod, log.mod1, log.mod2, log.mod3, log.mod4, log.mod5, 
-      log.mod6, log.mod7, log.mod8, log.mod9) #H_0 hypothesis is accepted
+plot(mod2)
+anova(model3, model4, model5, model6, model7, model8, model9, mod1, mod2) #H_0 hypothesis is accepted
+anova(model3, mod2)
